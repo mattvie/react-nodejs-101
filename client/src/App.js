38db537a423fd6622e1b8fba711530cdda165e12
mattvie <mvmf@cin.ufpe.br>
2024-04-04 17:31:05 -0300

    Minor changes
    App className was renamed to Index

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route index element={
-        <div className="App">
+        <div className="Index">
           <Header />
 
           <MainPageBody />
